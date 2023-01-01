8486202014183ec2664275ee58fbfdd4ff1282d2
glory jacques <101422810+lonniemoss@users.noreply.github.com>
2023-01-01 18:05:30 -0500

    work to do on smart contracts

---
+++ src/frontend/components/Create.js
@@ -0,0 +1,71 @@
+import './App.css';
+
+import { useState } from 'react'
+import { ethers } from "ethers"
+import { Row, Form, Button } from 'react-bootstrap'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
+const Create = ({ marketplace, nft }) => {
+  const [image, setImage] = useState('')
+  const [price, setPrice] = useState(null)
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const uploadToIPFS = async (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    if (typeof file !== 'undefined') {
+      try {
+        const result = await client.add(file)
+        console.log(result)
+        setImage(`https://ipfs.infura.io/ipfs/${result.path}`)
+      } catch (error){
+        console.log("ipfs image upload error: ", error)
+      }
+    }
+  }
+  const createNFT = async () => {
+    if (!image || !price || !name || !description) return
+    try{
+      const result = await client.add(JSON.stringify({image, price, name, description}))
+      mintThenList(result)
+    } catch(error) {
+      console.log("ipfs uri upload error: ", error)
+    }
+  }
+  const mintThenList = async (result) => {
+    const uri = `https://ipfs.infura.io/ipfs/${result.path}`
+    // mint nft 
+    await(await nft.mint(uri)).wait()
+    // get tokenId of new nft 
+    const id = await nft.tokenCount()
+    // approve marketplace to spend nft
+    await(await nft.setApprovalForAll(marketplace.address, true)).wait()
+    // add nft to marketplace
+    const listingPrice = ethers.utils.parseEther(price.toString())
+    await(await marketplace.makeItem(nft.address, id, listingPrice)).wait()
+  }
+
+
+  return (
+    <div className="box">
+      <h2 className="header">
+        Create
+      </h2>
+        <main role="main" className="col-lg-12 mx-auto" style={{ maxWidth: '1000px' }}>
+          <div className="drop-zone">          
+              <Row className="g-4">
+                <Form.Control type="file" required name="file" className="drop-zone__input" onChange={uploadToIPFS} />            
+                <div className="d-grid px-0">
+                <Button onClick={createNFT} variant="primary" size="lg">
+                  Create & File
+                </Button>
+              </div>
+            </Row>
+          </div>
+        </main>
+    </div>
+  );
+}
+
+export default Create
